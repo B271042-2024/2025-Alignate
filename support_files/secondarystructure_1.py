d58a53f65bb4d42da0e7d884ed6a19f9e6a87412
B271042-2024 <s2704130@login01.ecdf.ed.ac.uk>
2025-05-29 20:04:14 +0100

    250529_Alignate_protein_1_null

---
+++ support_files/secondarystructure_1.py
@@ -0,0 +1,11 @@
+# perform secondary structure
+# transfer to GUI
+# annotation for components on secondary structure with %similarity against ref
+from PySide6.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QSlider
+from alignate import 
+
+class secondarystructure(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        
